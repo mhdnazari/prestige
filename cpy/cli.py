e677d4530b6556ad3a38660951d6c85f42c0eb89
mahdinazari <mehdinazari@carrene.com>
2019-07-29 12:04:40 +0430

    Multi file support added and verbose bugs fixed

--- cpy/cli.py
+++ cpy/cli.py
@@ -8,13 +8,15 @@ from .linuxcp import recursive_copy, copyfile
 class CP(Root):
     __help__ = 'easycli example'
     __command__ = 'cp'
-    __completion__ = True
+    __completion__ = False
     __arguments__ = [
-#        Argument(
-#            '-V', '--version',
-#            action='version',
-#            version='CP 1.0',
-#        ),
+        Argument(
+            'files',
+            nargs='*',
+            metavar='N',
+            type=str,
+            help='source path'
+        ),
         Argument(
             '-r', '--recursive',
             action='store_true',
@@ -22,87 +24,117 @@ class CP(Root):
         ),
         Argument(
             '-v', '--verbose',
-            help='asdf',
+            help='explain what is being done',
             action='store_true',
-        ),
-        Argument(
-            'source',
-            help='source path'
-        ),
-        Argument(
-            'destination',
-            help='destination path'
         ),
     ]
 
+    def _split_source_destination(self, args):
+        if len(args.files) < 2:
+            print('Source or destination is not entered')
+            sys.exit()
+        sources = args.files[:-1]
+        destination = args.files[-1]
+        return sources, destination
+
     def __call__(self, args):
-        if not (os.path.exists(args.source) or os.path.isfile(args.source)):
-            print(
-                f"cp: cannot stat '{args.source}': No such file or directory"
-            )
+        sources, destination = self._split_source_destination(args)
+        current_dir = os.getcwd()
+        for source_file in sources:
 
-        if os.path.isdir(args.source):
-            if not args.recursive:
-                destination = os.path.abspath(args.destination)
-                destination = destination.split('/')[-1]
-                print(
-                    f"cp: -r not specified; omitting directory {destination!r}"
-                )
-                sys.exit()
+            if '/' in source_file[:-1]:
+                source_path = source_file
+                os.chdir(source_file[:source_file.rindex('/')])
+                file_name = source_file[source_file.rindex('/')+1:]
+
             else:
-                source = os.path.abspath(args.source)
-                destination = os.path.abspath(args.destination)
-                if '/' not in source[-1] or '/' not in source:
-                    top = destination + '/' + source[source.rindex('/')+1:]
+                source_path = current_dir + '/' + source_file
+                file_name = source_file
 
-                else:
-                    top = destination + '/' + source
-
-                if not os.path.isdir(args.destination):
-                    os.mkdir(args.destination)
-
-                if source == top:
-                    if '/' in destination:
-                        print(
-                            f"cp: '{args.source}' and ./'{destination}'"
-                            "are the same file"
-                        )
-                    else:
-                        print(
-                            f"cp: '{args.source}' and '{args.destination}'"
-                            "are the same file"
-                        )
-                    sys.exit()
-                if os.path.exists(top):
-                    for root, dirs, files in os.walk(top, topdown=False):
-                        for name in files:
-                            os.remove(os.path.join(root, name))
-                        for name in dirs:
-                            os.rmdir(os.path.join(root, name))
-                    os.rmdir(top)
-                recursive_copy(source, destination, args.verbose)
-
-        elif os.path.isfile(args.source):
-            source = args.source
-            destination = args.destination
-            if not(os.path.exists(source)):
+            if not (
+                os.path.exists(source_path) or os.path.isfile(source_path)
+            ):
                 print(
-                    f"cp: cannot stat '{args.source}':"
+                    f"cp: cannot stat '{source_path}':"\
                     "No such file or directory"
                 )
-            if not os.path.isdir(args.destination):
-                os.mkdir(args.destination)
-            if destination == "":
-                print(
-                    f"cp: missing destination file operand after "
-                    "'{args.destination}'"
-                )
-            if os.path.isdir(source):
-                print(
-                    f"cp: -r not specified; omitting directory '{args.source}'"
-                )
 
-            copyfile(source, destination, args.verbose)
+            if os.path.isdir(source_path):
+
+                if not args.recursive:
+                    destination = os.path.abspath(destination)
+                    destination = destination.split('/')[-1]
+                    print(
+                        f"cp: -r not specified;"\
+                        "omitting directory {destination!r}"
+                    )
+                    sys.exit()
+
+                else:
+                    source = os.path.abspath(source_path)
+                    destination = os.path.abspath(destination)
+
+                    if '/' not in source[-1] or '/' not in source:
+                        top = destination + '/' + source[source.rindex('/')+1:]
+
+                    else:
+                        top = destination + '/' + source
+
+                    if not os.path.isdir(destination):
+                        os.mkdir(destination)
+
+                    if source == top:
+
+                        if '/' in destination:
+                            print(
+                                f"cp: '{source_path}' and ./'{destination}'"
+                                "are the same file"
+                            )
+
+                        else:
+                            print(
+                                f"cp: '{source_path}' and '{destination}'"
+                                "are the same file"
+                            )
+                        sys.exit()
+                    if os.path.exists(top):
+
+                        for root, dirs, files in os.walk(top, topdown=False):
+
+                            for name in files:
+                                os.remove(os.path.join(root, name))
+
+                            for name in dirs:
+                                os.rmdir(os.path.join(root, name))
+                        os.rmdir(top)
+
+                    recursive_copy(source, destination, args.verbose)
+
+            elif os.path.isfile(source_path):
+                source = source_path
+                destination = destination
+
+                if not(os.path.exists(source)):
+                    print(
+                        f"cp: cannot stat '{source_path}':"
+                        "No such file or directory"
+                    )
+
+                if not os.path.isdir(destination):
+                    os.mkdir(destination)
+
+                if destination == "":
+                    print(
+                        f"cp: missing destination file operand after "
+                        "'{destination}'"
+                    )
+
+                if os.path.isdir(source):
+                    print(
+                        f"cp: -r not specified;"\
+                        "omitting directory '{source_path}'"
+                    )
+                copyfile(source, destination, args.verbose)
 
 
 if __name__ == '__main__':
